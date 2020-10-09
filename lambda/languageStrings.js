30ddf5dc668e6aeaa95f83f6e5af9af16fcdeb5b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-09 13:07:45 +0000

    updated lambda/languageStrings.js.

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -18,11 +18,8 @@ module.exports = {
             STOP_MESSAGE: 'Goodbye!',
             FACTS:
             [
-                'A year on Mercury is just 88 days long.',
-                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-                'On Mars, the Sun appears about half the size as it does on Earth.',
-                'Jupiter has the shortest day of all the planets.',
-                'The Sun is an almost perfect sphere.',
+                'Hacktoberfest takes place during October',
+                'Hacktoberfest is presented by Digital Ocean , DEV and Intel.',
             ],
         }
     },
